2d885b5613b80f555a4bb0515ee3110f752102dd
Jojo <jonathansardeamusa@gmail.com>
2022-06-20 15:44:31 +0800

    made working playOneround function

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-//basic rock paper scissors game
+const rpsButton = document.getElementsByClassName('rps-button');
+const scoreBox = document.getElementById('score-box');
 
+// use loop since rpsButton is a node list
+for (button of rpsButton) {
+    button.addEventListener('click',playOneRound);
+}
+
+// plays one round of rps
+function playOneRound(e) {
+    if (e.target.id !== '') {
+        let move = e.target.id;
+        let computerMove = computerPlay();
+        compare(move,computerMove);
+    }  
+}
 //computer move: get random integer among 0-2,
 //if random int equals 0 computer move is rock, if 1 move is paper, if 2 move is scissors
 
@@ -16,35 +30,34 @@ function computerPlay() {
     return computerSelection;
 }
 
+// get random integer
 function getRandomInt (max) {
     return Math.floor(Math.random()*(max+1));
 }
 
-let computerMove = computerPlay();
-let playerMove;
-console.log(computerMove);
+//compares playerSelection and computerSelection to see who wins
 
-//one round of rps: compare player move and computer move
-//use if statements, return flavor text on defeat or victory
-
-function playOneRound (playerSelection,computerSelection) {
+function compare (playerSelection,computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
     if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
-        return "defeat";//`Defeat! Your ${playerSelection} is no match against ${computerSelection}.`;
+        console.log("defeat");//`Defeat! Your ${playerSelection} is no match against ${computerSelection}.`;
     } 
     else if (computerSelection === "rock" && playerSelection === "paper" || computerSelection === "paper" && playerSelection === "scissors" || computerSelection === "scissors" && playerSelection === "rock") {
-        return "victory";//`Victory! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()} destroys ${computerSelection}.`;
+        console.log ("victory");//`Victory! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()} destroys ${computerSelection}.`;
     }
     else if (playerSelection === computerSelection){
-        return "tie";//`No winner! You've both used ${playerSelection}.`;
+        console.log("tie");//`No winner! You've both used ${playerSelection}.`;
     }
     else {
-    return "hmm"; //"Please enter your move.";
+    console.log("hmm"); //"Please enter your move.";
     }
 }
-console.log(playOneRound(playerMove,computerMove));
 
-//multiple rounds of rps: do a round of rps until one wins 5
-//use loops
+// console.log(playOneRound(playerMove,computerMove));
+
+// multiple rounds of rps: do a round of rps until one wins 5
+// use loops
 
 function game () {
     let playerScore = 0,
@@ -74,4 +87,4 @@ function game () {
     else if (computerScore === 5)
         return "Defeat....";
 }
-console.log(game());
+// console.log(game());
